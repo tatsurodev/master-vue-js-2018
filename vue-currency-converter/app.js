11a2fff7e6f2742fa6fc28a06923e981c534248f
Tatsuro <contacts@tatsuro.dev>
2019-06-28 04:37:46 +0900

    通貨リストをlocalStorageにキャッシュしてapi requestを抑える 11

--- vue-currency-converter/app.js
+++ vue-currency-converter/app.js
@@ -4,11 +4,29 @@ new Vue({
         currencies: {}
     },
     mounted() {
+        this.getCurrencires()
+    },
+    methods: {
         // 通貨リスト取得
         // https://free.currencyconverterapi.com/
         // api key 237d851531b84315a6ef
-        axios.get('https://free.currconv.com/api/v7/currencies?apiKey=237d851531b84315a6ef').then(response => {
-            this.currencies = response.data.results
-        })
+        getCurrencires() {
+            // apiのリクエストを抑えるために、頻繁に変わらない通貨リストをlocalStorageにキャッシュする
+            // WebStorage API(LocalStorage)をチェックしてcurrenciesの値を格納
+            const currencies = localStorage.getItem('currencies')
+            // currenciesがあれば
+            if (currencies) {
+                // JSON.parseでjsonをオブジェクトにデコード
+                this.currencies = JSON.parse(currencies)
+                return
+            }
+            // currenciesがなければ通貨リスト取得
+            axios.get('https://free.currconv.com/api/v7/currencies?apiKey=237d851531b84315a6ef').then(response => {
+                this.currencies = response.data.results
+                // JSON.stringifyでオブジェクトをjsonにエンコードしてlocalStorageに格納
+                // JSON.stringifyは、JSON.parseの逆
+                localStorage.setItem('currencies', JSON.stringify(response.data.results))
+            })
+        }
     }
 })
