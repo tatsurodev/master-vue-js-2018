26e471a130a6c38fd5769ba61b90bf0601b63695
Tatsuro <contacts@tatsuro.dev>
2019-06-29 07:55:39 +0900

    Navbar作成、root componentにinstall 34

--- community-blog/src/App.vue
+++ community-blog/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
+    <Navbar/>
     <!-- routerで設定したcomponentがrouter-viewに表示 -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Navbar from "@/components/Navbar.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Navbar
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: "Poppins";
+  border-radius: 0px !important;
+}
+body {
+  background-color: #f5f3ef;
 }
 </style>
