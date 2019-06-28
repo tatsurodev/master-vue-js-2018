590876ff9f2dcb511e98a7cc5726f582f9dbe04e
Tatsuro <contacts@tatsuro.dev>
2019-06-28 13:00:05 +0900

    card componentにprops作成 19

--- vue-components/js/app.js
+++ vue-components/js/app.js
@@ -1,13 +1,17 @@
 // componentの第二引数のobject内ではvue instanceで使えるオプションが使える
 Vue.component('card', {
+    // propertiesは配列で指定
+    props: ['title', 'content'],
     // templateにはテンプレートリテラルの`を使うと複数行のヒアドキュメントが使える
     template: `
 <div class="card">
     <div class="card-body">
         <h3 class="card-title">
-            Vuejs fundamentals
+            {{ title }}
         </h3>
-        <div class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae amet consequatur suscipit, exercitationem quisquam sequi quo facilis molestiae repudiandae sed ea rerum accusantium rem ipsum iure natus consectetur, ipsa itaque!</div>
+        <div class="card-text">
+            {{ content }}
+        </div>
     </div>
 </div>
 `
