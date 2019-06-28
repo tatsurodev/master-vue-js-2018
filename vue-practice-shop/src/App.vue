6537f13135a98476780de0db8ac2f232707eb733
Tatsuro <contacts@tatsuro.dev>
2019-06-28 21:26:58 +0900

    Product component作成 26

--- vue-practice-shop/src/App.vue
+++ vue-practice-shop/src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-7"></div>
+      <div class="col-md-7">
+        <div class="row">
+          <div :key="product.id" class="col-md-6" v-for="product in Products">
+            <product></product>
+          </div>
+        </div>
+      </div>
       <div class="col-md-5"></div>
     </div>
   </div>
 </template>
 
 <script>
+import Products from "@/products.json";
+import Product from "@/components/Product.vue";
+
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    Product
+  },
+  data() {
+    return {
+      Products
+    };
+  }
 };
 </script>
 
 <style>
 body {
   background-color: #fbf8f3;
-  background-color: red;
 }
 </style>
