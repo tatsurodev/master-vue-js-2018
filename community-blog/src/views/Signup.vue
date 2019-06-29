f758362e8e78e0d233d5986fdae55716b1cc6525
Tatsuro <contacts@tatsuro.dev>
2019-06-30 03:54:29 +0900

    Signup componentの各inputにv-model紐付け 37

--- community-blog/src/views/Signup.vue
+++ community-blog/src/views/Signup.vue
@@ -5,19 +5,35 @@
         <div class="card-body">
           <h3 class="text-center my-5">Signup</h3>
           <div class="form-group">
-            <input type="text" placeholder="Name" class="form-control">
+            <input v-model="name" type="text" placeholder="Name" class="form-control">
           </div>
           <div class="form-group">
-            <input type="text" placeholder="Email" class="form-control">
+            <input v-model="email" type="text" placeholder="Email" class="form-control">
           </div>
           <div class="form-group">
-            <input type="passowrd" placeholder="Password" class="form-control">
+            <input v-model="password" type="passowrd" placeholder="Password" class="form-control">
           </div>
           <div class="form-group text-center">
-            <button class="btn btn-success form-control">Signup</button>
+            <button @click="registerUser()" class="btn btn-success form-control">Signup</button>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    registerUser() {
+      console.log(this.name, this.email, this.password);
+    }
+  }
+};
+</script>
