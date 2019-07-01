02168f778f0423adf2e38f53b6fc72ab3eed5e0d
Tatsuro <contacts@tatsuro.dev>
2019-07-01 18:15:30 +0900

    Home componentにArticle componentをv-forで繰り返し 48

--- community-blog/src/views/Home.vue
+++ community-blog/src/views/Home.vue
@@ -1,3 +1,40 @@
 <template>
-  <h1 class="text-cneter my-5">Welcome to the home screen</h1>
+  <div class="row">
+    <div
+      class="col-md-8 offset-md-2"
+      v-if="articles.data"
+      v-for="article in articles.data"
+      :key="article.id"
+    >
+      <Article/>
+    </div>
+  </div>
 </template>
+
+<script>
+import Axios from "axios";
+import config from "@/config";
+import Article from "@/components/Article.vue";
+
+export default {
+  components: {
+    Article
+  },
+  mounted() {
+    this.getArticles();
+  },
+  data() {
+    return {
+      articles: {}
+    };
+  },
+  methods: {
+    getArticles() {
+      Axios.get(`${config.apiUrl}/articles`).then(response => {
+        // console.log(response);
+        this.articles = response.data.data;
+      });
+    }
+  }
+};
+</script>
