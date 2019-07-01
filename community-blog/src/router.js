2a32597cd5b3467cca7b0f318139b35db768ffe9
Tatsuro <contacts@tatsuro.dev>
2019-07-02 00:10:14 +0900

    dynamic paramをrouterに設定、個別記事を表示するArticle.vue作成 50

--- community-blog/src/router.js
+++ community-blog/src/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Signup from './views/Signup.vue'
+import Article from './views/Article.vue'
 
 // pluginをvueに登録
 Vue.use(Router)
@@ -21,6 +22,10 @@ const router = new Router({
         {
             path: '/',
             component: Home
+        },
+        {
+            path: '/article/:id',
+            component: Article
         }
     ]
 })
---
+++ community-blog/src/views/Article.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- params_nameはrouter.jsで設定したもの -->
+  <h1 class="text-center">{{ $route.params.id }}</h1>
+</template>
+<script>
+import Axios from "axios";
+import config from "@/config";
+
+export default {
+  mounted() {
+    this.getArticle();
+  },
+  methods: {
+    getArticle() {
+      // $routeにscript内でアクセスするにはthisが必要
+      Axios.get(`${config.apiUrl}/article/${this.$route.params.id}`).then(
+        response => {
+          console.log(response);
+        }
+      );
+    }
+  }
+};
+</script>
