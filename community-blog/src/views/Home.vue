e5b91f3f9360a7d23609ec9b729a3ccffc257b79
Tatsuro <contacts@tatsuro.dev>
2019-07-02 23:54:46 +0900

    Home vueにpagination実装 53

--- community-blog/src/views/Home.vue
+++ community-blog/src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="row">
-    <div
-      class="col-md-8 offset-md-2"
-      v-if="articles.data"
-      v-for="article in articles.data"
-      :key="article.id"
-    >
-      <Article :article="article"/>
+  <div>
+    <div class="d-flex mt-4 justify-content-between">
+      <button
+        @click="getPrevArticles()"
+        :disabled="articles.prev_page_url === null"
+        class="btn btn-warning"
+      >Prev Page</button>
+      <button
+        @click="getNextArticles()"
+        :disabled="articles.next_page_url === null"
+        class="btn btn-warning"
+      >Next Page</button>
+    </div>
+    <div class="row" v-if="!loading">
+      <div class="col-md-8 offset-md-2" v-for="article in articles.data" :key="article.id">
+        <Article :article="article"/>
+      </div>
+    </div>
+    <div class="loader text-center" v-else>
+      <i class="fas fa-3x fa-spin fa-spinner"></i>
     </div>
   </div>
 </template>
@@ -25,16 +37,30 @@ export default {
   },
   data() {
     return {
-      articles: {}
+      articles: {},
+      loading: true
     };
   },
   methods: {
-    getArticles() {
-      Axios.get(`${config.apiUrl}/articles`).then(response => {
+    getArticles(url = `${config.apiUrl}/articles`) {
+      this.loading = true;
+      Axios.get(url).then(response => {
+        this.loading = false;
         // console.log(response);
         this.articles = response.data.data;
       });
+    },
+    getNextArticles() {
+      this.getArticles(this.articles.next_page_url);
+    },
+    getPrevArticles() {
+      this.getArticles(this.articles.prev_page_url);
     }
   }
 };
 </script>
+<style>
+.btn-warning {
+  color: #fff;
+}
+</style>
