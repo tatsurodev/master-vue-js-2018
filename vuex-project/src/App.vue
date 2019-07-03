1731d164d0e6a2a22837dd9959b976fdcfde8212
Tatsuro <contacts@tatsuro.dev>
2019-07-03 21:35:04 +0900

    root componentにsaveTodo method作成して、追加したtodosを表示

--- vuex-project/src/App.vue
+++ vuex-project/src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="todo" @keyup.enter="saveTodo()" type="text" placeholder="Todo item" />
+    <ul>
+      <li v-for="(todoitem, index) in todos" :key="index">{{ todoitem }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: () => ({
+    todo: "",
+    todos: []
+  }),
+  methods: {
+    saveTodo() {
+      // スプレッド演算子
+      // a = [1, 2, 3];
+      // console.log(...a)は、console.log(1, 2, 3)と同義
+      // 配列todosにtodoを追加
+      this.todos = [...this.todos, this.todo];
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input {
+  border: 2px solid black;
+  padding: 20px;
+  font-size: 22px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  font-size: 26px;
 }
 </style>
