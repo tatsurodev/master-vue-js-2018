ba548e9f1da6fd47d7cf4f2b122ee56703d2fce5
Tatsuro <contacts@tatsuro.dev>
2019-07-03 20:54:08 +0900

    vuexのinstallと登録

--- vuex-project/src/main.js
+++ vuex-project/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // vue instanceのオプションとしてstore
+  store,
   render: h => h(App),
 }).$mount('#app')
